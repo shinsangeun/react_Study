09844357c2b41795e124e50b992c7be40b0851d1
shinsangeun <s0813se@naver.com>
2019-12-08 23:58:42 +0900

    componentDidMount 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,30 +3,51 @@ import { Component } from 'react'
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-    {
-        title: "Matrix",
-        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-    },
-    {
-        title: "Frozen",
-        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-    },
-    {
-        title: "Oldboy",
-        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-    },
-    {
-        title: "기생충",
-        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-    }
-]
-
 class App extends Component {
+
+    //Render: componentWillMount() -> render() -> componentDidMount()
+    //Update
+
+    state = {
+        greeting: 'Hello',
+        movies: [
+            {
+                title: "Matrix",
+                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+            },
+            {
+                title: "Frozen",
+                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+            },
+            {
+                title: "Oldboy",
+                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+            },
+            {
+                title: "기생충",
+                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+            }
+        ]
+    };
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                movies:[
+                ...this.state.movies,
+                {title: "test",
+                poster:"http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+                }
+                ]
+            })
+        },1000)
+    }
+
     render() {
         return (
             <div className="App">
-                {movies.map((movie, index) => {
+                {this.state.greeting}
+                {this.state.movies.map((movie, index) => {
                     return <Movie title = {movie.title} poster = {movie.poster} key={index}/>
                 })}
             </div>
