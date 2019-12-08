900ea6ccccab0deec35c914a27e589a6220b2f00
shinsangeun <s0813se@naver.com>
2019-12-09 00:11:08 +0900

    Add loading state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Component } from 'react'
+import {Component} from 'react'
 import './App.css';
 import Movie from './Movie';
 
@@ -8,48 +8,49 @@ class App extends Component {
     //Render: componentWillMount() -> render() -> componentDidMount()
     //Update
 
-    state = {
-        greeting: 'Hello',
-        movies: [
-            {
-                title: "Matrix",
-                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-            },
-            {
-                title: "Frozen",
-                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-            },
-            {
-                title: "Oldboy",
-                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-            },
-            {
-                title: "기생충",
-                poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-            }
-        ]
-    };
+    state = {};
 
     componentDidMount() {
-        setTimeout(()=>{
+        setTimeout(() => {
             this.setState({
-                movies:[
-                ...this.state.movies,
-                {title: "test",
-                poster:"http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
-                }
+                movies: [
+                    {
+                        title: "Matrix",
+                        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+                    },
+                    {
+                        title: "Frozen",
+                        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+                    },
+                    {
+                        title: "Oldboy",
+                        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+                    },
+                    {
+                        title: "기생충",
+                        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+                    },
+                    {
+                        title: "test",
+                        poster: "http://images.christiantoday.co.kr/data/images/full/323947/2.jpg?w=654"
+                    }
+
                 ]
             })
-        },1000)
+        }, 4000)
     }
+
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie, index) => {
+            return <Movie title={movie.title} poster={movie.poster} key={index}/>
+        });
+        return movies
+    };
 
     render() {
         return (
             <div className="App">
-                {this.state.greeting}
-                {this.state.movies.map((movie, index) => {
-                    return <Movie title = {movie.title} poster = {movie.poster} key={index}/>
-                })}
+                {this.state.movies ? this._renderMovies() : 'Loading...'}
             </div>
         );
     }
