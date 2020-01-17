f4082a582c0c06aa032fd3c13082932e404b83ab
shinsangeun <s0813se@naver.com>
2020-01-17 17:42:23 +0900

    Updateing Movie

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ class App extends Component {
 
     _renderMovies = () => {
         const movies = this.state.movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+            return <Movie
+                title = {movie.title}
+                poster = {movie.medium_cover_image}
+                key = {movie.id}
+                genres = {movie.genres}
+                synopsis = {movie.synopsis}
+            />
         });
         return movies
     };
