16abcd021b2f8853facbd2c0b16d17530b24fedd
shinsangeun <s0813se@naver.com>
2020-02-15 17:36:29 +0900

    1. Update css 2. Add components/routes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
-import React from 'react';
-import {Component} from 'react'
-import './App.css';
-import Movie from './Movie';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-class App extends Component {
-
-    //Render: componentWillMount() -> render() -> componentDidMount()
-    //Update
-
-    state = {};
-
-    componentDidMount() {
-        this._getMovies();
-    }
-
-    _renderMovies = () => {
-        const movies = this.state.movies.map((movie, index) => {
-            return <Movie
-                title = {movie.title}
-                poster = {movie.medium_cover_image}
-                key = {movie.id}
-                genres = {movie.genres}
-                synopsis = {movie.synopsis}
-            />
-        });
-        return movies
-    };
-
-     _getMovies = async () => {
-        const movies = await this._callApi();
-        this.setState({
-            movies
-        })
-    };
-
-    _callApi = () => {
-        return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
-            .then(response => response.json())
-            .then(json =>  json.data.movies)
-            .catch(err => console.log(err))
-    };
-
-    render() {
-        return (
-            <div className="App">
-                {this.state.movies ? this._renderMovies() : 'Loading...'}
-            </div>
-        );
-    }
+function App() {
+    return (
+        <HashRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/movie/:id" component={Detail} />
+        </HashRouter>
+    );
 }
 
 export default App;
